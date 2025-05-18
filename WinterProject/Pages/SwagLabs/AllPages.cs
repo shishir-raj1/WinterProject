41aec71b52d1b534836a7ba914e901fa89a0e1a7
SHISHIR RAJ <shishirraj1997@gmail.com>
2025-05-18 15:10:13 +0530

    structure

---
+++ WinterProject/Pages/SwagLabs/AllPages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using WinterProject.Pages.SwagLabs.LoginPage;
+
+namespace WinterProject.Pages.SwagLabs
+{
+    public class AllPages
+    {
+        private IWebDriver driver;
+
+        public AllPages(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+       // LoginPage loginPage = new LoginPage();
+
+        
+
+
+    }
+}
---
+++ WinterProject/Pages/SwagLabs/LoginPage/LoginPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+
+namespace WinterProject.Pages.SwagLabs.LoginPage
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+        private readonly string _userNameId = "user-name";
+        private readonly string _passwordId = "password";
+        private readonly string _loginId = "login-button";
+
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void enterUsername(string username)
+        {
+            IWebElement userName = driver.FindElement(By.Id(_userNameId));
+            userName.SendKeys(username);
+        }
+
+        public void enterPassword(string password)
+        {
+            IWebElement passWord = driver.FindElement(By.Id(_passwordId));
+            passWord.SendKeys(password);
+        }
+
+        public void clickLoginButton()
+        {
+            IWebElement loginButton = driver.FindElement(By.Id(_loginId));
+            loginButton.Click();
+        }
+    }
+}
