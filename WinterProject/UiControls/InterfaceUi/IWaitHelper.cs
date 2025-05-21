a41d6692289f93728d0f646bb6004028e829e3f2
SHISHIR RAJ <shishirraj1997@gmail.com>
2025-05-22 00:32:55 +0530

    interface class

---
+++ WinterProject/UiControls/InterfaceUi/IWaitHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace WinterProject.UiControls.InterfaceUi
+{
+    public interface IWaitHelper
+    {
+        IWebElement WaitUntilVisible(By locator, int timeoutInSeconds = 10);
+        IWebElement WaitUntilClickable(By locator, int timeoutInSeconds = 10);
+    }
+
+}
